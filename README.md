e41f96eb9fcde8b5ad0ce80f27ece08b4f10bd3e
pincoin <pincoins@gmail.com>
2018-01-08 01:01:02 +0900

    - attachment model added

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# django-jquery-fileupload
-jQuery fileupload based on Django
+# Introduction
+
+jquery-fileupload sample code based on Django 2.0
+
+# Features
+
+* How to use FileField, ModelForm, and FormView
+* How to use jquery-fileupload
+
+# Requirements
+
+* [jQuery-File-Upload](http://aquantum-demo.appspot.com/file-upload) developed by Sebastian Tschan ([github](https://github.com/blueimp/jQuery-File-Upload))
+* Django 2.0 or above
+* Pillow 5.0 or above
+
+# Installation
+
+* pip install -r requirements.txt
+* python manage.py migrate
+* python manage.py runserver
+
+# License
+
+MIT, as the original project. See LICENSE.txt.
--- fileupload/models.py
+++ fileupload/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Attachment(models.Model):
+    file = models.ImageField(upload_to="attachment")
+    slug = models.SlugField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.file.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('upload-new',)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.file.name, allow_unicode=True)
+
+        super(Attachment, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        # remove a file from storage
+        self.file.delete(False)
+
+        super(Attachment, self).delete(*args, **kwargs)
