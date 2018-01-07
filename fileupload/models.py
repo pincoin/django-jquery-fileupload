55a7985d71ae980855312c0eaba46433abd567e4
pincoin <pincoins@gmail.com>
2018-01-08 01:45:00 +0900

    - post model added

--- fileupload/models.py
+++ fileupload/models.py
@@ -1,17 +1,60 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 
 
+class Post(models.Model):
+    title = models.CharField(
+        verbose_name='title',
+        max_length=255,
+        help_text="The page title as you'd like it to be seen by the public",
+    )
+
+    body = models.TextField(
+        verbose_name='content body',
+    )
+
+    slug = models.SlugField(
+        verbose_name='slug',
+        help_text='A short label containing only letters, numbers, underscores or hyphens for URL',
+        max_length=255,
+        unique=True,
+        allow_unicode=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('fileupload:post-detail', args=[self.slug, ])
+
+
 class Attachment(models.Model):
-    file = models.ImageField(upload_to="attachment")
-    slug = models.SlugField(max_length=50, blank=True)
+    file = models.ImageField(
+        verbose_name='uploaded file',
+        upload_to="attachment",
+    )
+
+    slug = models.SlugField(
+        verbose_name='slug',
+        help_text='A short label containing only letters, numbers, underscores or hyphens for URL',
+        max_length=255,
+        unique=True,
+        allow_unicode=True,
+    )
+
+    post = models.ForeignKey(
+        'fileupload.Post',
+        verbose_name='post',
+        related_name='attachments',
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.file.name
 
-    @models.permalink
     def get_absolute_url(self):
-        return ('upload-new',)
+        return reverse('fileupload:attachment-detail', args=[self.slug, ])
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.file.name, allow_unicode=True)
