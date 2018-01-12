ff48a896c86ecd5633e96959f5fe489e10d4d415
pincoin <pincoins@gmail.com>
2018-01-13 01:42:34 +0900

    - summernote image upload

--- sandbox/static/js/post-summernote.js
+++ sandbox/static/js/post-summernote.js
@@ -11,7 +11,25 @@ $(document).ready(function () {
                 $nImageInput = $nEditor.find('.note-image-input');
             },
             onImageUpload: function (files) {
-                console.log('image button clicked');
+                $nImageInput.fileupload();
+                var jqXHR = $nImageInput.fileupload('send', {
+                    url: '/upload',
+                    files: files
+                }).done(function (data, textStatus, jqXHR) {
+                    $.each(data.files, function (index, file) {
+                        $sn.summernote('insertImage', file.url);
+
+                        $("#files ul").append('<li>' + file.pk + ': <a href="' + file.url + '">' + file.name + '</a></li>');
+
+                        $("<input>", {
+                            type: "hidden",
+                            name: "attachments",
+                            value: file.pk
+                        }).appendTo("form");
+                    });
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    console.log('Failed to upload');
+                });
             }
         }
     })
